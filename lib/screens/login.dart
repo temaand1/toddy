576731fee5f59dda0c2108e2533f908270ce2185
temaand1 <45510216+temaand1@users.noreply.github.com>
2021-08-30 20:01:57 +0200

    bug fixes

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -85,148 +85,14 @@ class LoginState extends State<Login> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            TextButton(
-                              onPressed: () async {
-                                try {
-                                  // ignore: unused_local_variable
-                                  final user = await _auth
-                                      .createUserWithEmailAndPassword(
-                                          email: emailController.text,
-                                          password: passwordController.text);
-
-                                  Navigator.pushNamed(context, '/');
-                                } on FirebaseException catch (e) {
-                                  if (e.code == 'invalid-email') {
-                                    showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            title: Text("Error"),
-                                            content: Text(
-                                                'Please enter correct email adress'),
-                                            actions: [
-                                              TextButton(
-                                                child: Text("Ok"),
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                              )
-                                            ],
-                                          );
-                                        });
-                                  } else if (e.code == 'weak-password') {
-                                    showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            title: Text("Error"),
-                                            content: Text(
-                                                'The password is too weak'),
-                                            actions: [
-                                              TextButton(
-                                                child: Text("Ok"),
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                              )
-                                            ],
-                                          );
-                                        });
-                                  } else if (e.code == 'email-already-in-use') {
-                                    showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            title: Text("Error"),
-                                            content:
-                                                Text('Email already in use'),
-                                            actions: [
-                                              TextButton(
-                                                child: Text("Ok"),
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                              )
-                                            ],
-                                          );
-                                        });
-                                  }
-                                }
-                              },
-                              child: Container(
-                                  width: 80,
-                                  height: 40,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(5),
-                                      color: kAccentColor),
-                                  child: Center(
-                                    child: Text(
-                                      'REGISTER',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  )),
-                            ),
-                            TextButton(
-                              onPressed: () async {
-                                try {
-                                  // ignore: unused_local_variable
-                                  final user =
-                                      await _auth.signInWithEmailAndPassword(
-                                          email: emailController.text,
-                                          password: passwordController.text);
-
-                                  Navigator.pushNamed(context, '/');
-                                } on FirebaseException catch (e) {
-                                  if (e.code == 'user-not-found') {
-                                    showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            title: Text("Error"),
-                                            content: Text('User not found'),
-                                            actions: [
-                                              TextButton(
-                                                child: Text("Ok"),
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                              )
-                                            ],
-                                          );
-                                        });
-                                  } else if (e.code == 'wrong-password') {
-                                    showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            title: Text("Error"),
-                                            content: Text('Wrong password'),
-                                            actions: [
-                                              TextButton(
-                                                child: Text("Ok"),
-                                                onPressed: () {
-                                                  Navigator.of(context).pop();
-                                                },
-                                              )
-                                            ],
-                                          );
-                                        });
-                                  }
-                                }
-                              },
-                              child: Container(
-                                  width: 80,
-                                  height: 40,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(5),
-                                      color: kAccentColor),
-                                  child: Center(
-                                    child: Text(
-                                      'LOGIN',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  )),
-                            ),
+                            RegisterButton(
+                                auth: _auth,
+                                emailController: emailController,
+                                passwordController: passwordController),
+                            SingInButton(
+                                auth: _auth,
+                                emailController: emailController,
+                                passwordController: passwordController),
                           ],
                         ),
                       ],
@@ -238,5 +104,184 @@ class LoginState extends State<Login> {
                 ],
               ),
             )));
+  }
+}
+
+class SingInButton extends StatelessWidget {
+  const SingInButton({
+    Key? key,
+    required FirebaseAuth auth,
+    required this.emailController,
+    required this.passwordController,
+  })  : _auth = auth,
+        super(key: key);
+
+  final FirebaseAuth _auth;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () async {
+        try {
+          // ignore: unused_local_variable
+          final user = await _auth.signInWithEmailAndPassword(
+              email: emailController.text, password: passwordController.text);
+
+          Navigator.pushNamed(context, '/');
+        } on FirebaseException catch (e) {
+          if (e.code == 'user-not-found') {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    title: Text("Error"),
+                    content: Text('User not found'),
+                    actions: [
+                      TextButton(
+                        child: Text("Ok"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  );
+                });
+          } else if (e.code == 'wrong-password') {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    title: Text("Error"),
+                    content: Text('Wrong password'),
+                    actions: [
+                      TextButton(
+                        child: Text("Ok"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  );
+                });
+          }
+        }
+      },
+      child: Container(
+          width: 80,
+          height: 40,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5), color: kAccentColor),
+          child: Center(
+            child: Text(
+              'LOGIN',
+              style: TextStyle(color: Colors.white),
+            ),
+          )),
+    );
+  }
+}
+
+class RegisterButton extends StatelessWidget {
+  const RegisterButton({
+    Key? key,
+    required FirebaseAuth auth,
+    required this.emailController,
+    required this.passwordController,
+  })  : _auth = auth,
+        super(key: key);
+
+  final FirebaseAuth _auth;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () async {
+        try {
+          // ignore: unused_local_variable
+          final user = await _auth.createUserWithEmailAndPassword(
+              email: emailController.text, password: passwordController.text);
+
+          Navigator.pushNamed(context, '/');
+        } on FirebaseException catch (e) {
+          if (e.code == 'invalid-email') {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    title: Text("Error"),
+                    content: Text('Please enter correct email adress'),
+                    actions: [
+                      TextButton(
+                        child: Text("Ok"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  );
+                });
+          } else if (e.code == 'weak-password') {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    title: Text("Error"),
+                    content: Text('The password is too weak'),
+                    actions: [
+                      TextButton(
+                        child: Text("Ok"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  );
+                });
+          } else if (e.code == 'email-already-in-use') {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    title: Text("Error"),
+                    content: Text('Email already in use'),
+                    actions: [
+                      TextButton(
+                        child: Text("Ok"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  );
+                });
+          }
+        }
+      },
+      child: Container(
+          width: 80,
+          height: 40,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5), color: kAccentColor),
+          child: Center(
+            child: Text(
+              'REGISTER',
+              style: TextStyle(color: Colors.white),
+            ),
+          )),
+    );
   }
 }
